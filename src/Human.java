ff1ed8d81fd0ae544c5b4e46c906f0b2fc79312b
Dima <akmeev2011@yandex.ru>
2022-09-23 20:15:47 +0300

    дз3 задние 1

--- src/Human.java
+++ src/Human.java
@@ -7,27 +7,40 @@ public class Human {
     int yearOfBirth;
     String job;
 
-
-
     Human() {
-    }
-//    public Human(String name, String town, int age, String job) {
-//        int currentYear = LocalDate.now().getYear();
-//        this.name = name;
-//        this.town = town;
-//        this.yearOfBirth = currentYear - age;
-//        this.job = job;
-//    }
 
-//    void printHuman() {
-//        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(ась) в " + yearOfBirth +
-//                " году. Я работаю на должности " + job + ". Будем знакомы!");
-//    }
+    }
+
+    public Human(String name, String town, int age, String job) {
+        int currentYear = LocalDate.now().getYear();
+        if (name == null) {
+            this.name = "Информация не указана";
+        } else {
+            this.name = name;
+        }
+        if (job == null) {
+            this.job = "Информация не указана";
+        }else {
+            this.job = job;
+        }
+        if (town == null) {
+            this.town = "Информация не указана";
+        } else {
+            this.town = town;
+        }
+        this.yearOfBirth = currentYear - age;
+        if (yearOfBirth < 0) {
+            yearOfBirth = 0;
+        }
+
+    }
+
 
     @Override
     public String toString() {
-        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(ась) в " + (LocalDate.now().getYear() - age) +
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(ась) в " + yearOfBirth +
                 " году. Я работаю на должности " + job + ". Будем знакомы!";
     }
 
 }
+
--- src/Main.java
+++ src/Main.java
@@ -3,41 +3,18 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        Human human = new Human();
-        Human human1 = new Human();
-        Human human2 = new Human();
-        Human human3 = new Human();
-        human.name = "Максим";
-        human.town = "Минск";
-        human.age = 35;
-        human.job = "бренд-менеджер";
-        human1.name = "Артем";
-        human1.town = "Москва";
-        human1.age = 27;
-        human1.job = "директор по развитию бизнеса";
-        human2.name = "Катя";
-        human2.town = "Калининград";
-        human2.yearOfBirth = 28;
-        human2.job = "продакт-менеджер";
-        human3.name = "Аня";
-        human3.town = "Москва";
-        human3.yearOfBirth = 29;
-        human3.job = "методист образовательных программ";
+
+        Human human = new Human(null, "Минск", 35, "бренд-менеджер");
+        Human human1 = new Human("Аня", "Москва", 29, "методист образовательных программ");
+        Human human2 = new Human("Катя", "Калининград", 28, "продакт-менеджер");
+        Human human3 = new Human("Артем", "Москва", 27, "директор по развитию бизнеса");
+
+
         System.out.println(human);
         System.out.println(human1);
         System.out.println(human2);
         System.out.println(human3);
 
-
-//        human.printHuman();
-//        human1.printHuman();
-//        human2.printHuman();
-//        human3.printHuman();
-
-        //"Аня", "Москва", 29, "методист образовательных программ"
-        //"Катя", "Калининград", 28, "продакт-менеджер"
-        //"Артем", "Москва", 27, "директор по развитию бизнеса"
-        //"Максим", "Минск", 35, "бренд-менеджер"
 
 
     }
