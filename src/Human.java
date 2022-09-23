7b486381e7b8cf3f1f82259a983aa37a159571e6
Dima <akmeev2011@yandex.ru>
2022-09-23 18:58:44 +0300

    дз1 задние 1 и 2

--- src/Human.java
+++ src/Human.java
@@ -1,32 +1,33 @@
 import java.time.LocalDate;
 
 public class Human {
-    private String name;
-    private String town;
-    private int yearOfBirth;
-    private String job;
+    String name;
+    String town;
+    int age;
+    int yearOfBirth;
+    String job;
 
 
-    public Human(String name, String town, int age, String job) {
-        int currentYear = LocalDate.now().getYear();
-        this.name = name;
-        this.town = town;
-        this.yearOfBirth = currentYear - age;
-        this.job = job;
+
+    Human() {
     }
+//    public Human(String name, String town, int age, String job) {
+//        int currentYear = LocalDate.now().getYear();
+//        this.name = name;
+//        this.town = town;
+//        this.yearOfBirth = currentYear - age;
+//        this.job = job;
+//    }
 
-    void printHuman() {
-        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(ась) в " + yearOfBirth +
-                " году. Я работаю на должности " + job + ". Будем знакомы!");
-    }
+//    void printHuman() {
+//        System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(ась) в " + yearOfBirth +
+//                " году. Я работаю на должности " + job + ". Будем знакомы!");
+//    }
 
     @Override
     public String toString() {
-        return "Human{" +
-                "name='" + name + '\'' +
-                ", city='" + town + '\'' +
-                ", birthYear=" + yearOfBirth +
-                '}';
+        return "Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился(ась) в " + (LocalDate.now().getYear() - age) +
+                " году. Я работаю на должности " + job + ". Будем знакомы!";
     }
 
 }
